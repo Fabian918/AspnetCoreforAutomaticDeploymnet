d8b2e26aaee0ae57021c60d98baff27a336d340d
Fabian918 <fabian.hering140@gmail.com>
2021-03-06 16:44:56 +0100

    #first commit

---
+++ ASPNETCORE_ForContainering/HalloWeltAPI.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCORE_ForContainering
+{
+    [Controller]
+    [Route("api/")]
+    public class HalloWeltAPI
+    {
+        [HttpGet("hallo/{name}")]
+
+        public async Task<string> Hallo (string name )
+        {
+            return await Task.FromResult($"Hallo {name}!");
+        }
+
+
+    }
+}
